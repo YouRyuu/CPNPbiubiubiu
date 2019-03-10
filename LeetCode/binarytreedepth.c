f12d4883425b4c0b161751c71d992276e651be1e
youryuu <l9819y@gmail.com>
2019-03-10 22:40:21 +0800

    fuck

---
+++ LeetCode/binarytreedepth.c
@@ -0,0 +1,10 @@
+int maxDepth(struct TreeNode* root) {
+     if(root==NULL)
+        return 0;
+    int ldepth=maxDepth(root->left);
+    int rdepth=maxDepth(root->right);
+    if(ldepth>rdepth)
+        return ldepth+1;
+    else
+        return rdepth+1;
+}
