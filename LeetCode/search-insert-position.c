0b979b6ef01d4b7050b7391929464b4d0c86ee32
youryuu <l9819y@gmail.com>
2019-03-12 22:06:03 +0800

    fuck

---
+++ LeetCode/search-insert-position.c
@@ -0,0 +1,22 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+    int mid = (left + right)/2;
+    while(left<right)
+    {
+        if(nums[mid]<target)
+        {
+            left = mid + 1;
+            mid = (left + right)/2;
+        }
+        else if(nums[mid]>target)
+        {
+            right = mid - 1;
+            mid = (left + right)/2;
+        }
+        else return mid;
+    }
+    if(nums[left]<target) return left+1;
+    else if(nums[left]>target) return left;
+    else return left;
+}
