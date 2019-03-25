2d4dee802ef83562afb13de6c2a96570c810a6f7
youryuu <l9819y@gmail.com>
2019-03-25 21:55:11 +0800

    fuck

---
+++ LeetCode/array-partition-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        sums = 0
+        if len(nums) <= 2:
+            return nums[0]
+        length = len(nums)
+        for i in range(0,length,2):
+            sums = sums + nums[i]
+        return sums
