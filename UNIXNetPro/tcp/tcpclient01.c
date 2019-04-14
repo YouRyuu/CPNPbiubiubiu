7e84dad6d098c0d0b8b20b1dc3e20ea291a2c069
youryuu <l9819y@gmail.com>
2019-04-14 18:20:43 +0800

    select client

--- UNIXNetPro/tcp/tcpclient01.c
+++ UNIXNetPro/tcp/tcpclient01.c
@@ -1,32 +1,23 @@
 //
 // Created by youryuu on 19-4-7.
+// TCP客户端
 //
 
 #include "../lib/unnp.h"
 
-void tcpclient()
-{
-    char send[1024], recv[1024];
+void tcpclient() {
     int sockfd;
     struct sockaddr_in serveraddr;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = Socket(AF_INET, SOCK_STREAM, 0);
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(SEVE_PORT);
     inet_pton(AF_INET, "127.0.0.1", &serveraddr.sin_addr);
-    connect(sockfd, (SA *)&serveraddr, sizeof(serveraddr));
-    while(1)
-    {
-        printf("\nsend to server:");
-        fgets(send, 1024, stdin);
-        write(sockfd, send, strlen(send));
-        printf("\nrecv from server:");
-        read(sockfd, recv, 1024);
-        fputs(recv, stdout);
-    }
+    Connect(sockfd, (SA *) &serveraddr, sizeof(serveraddr));
+    str_cli(stdin, sockfd);
+    exit(0);
 }
 
-void main()
-{
+void main() {
     tcpclient();
 }
