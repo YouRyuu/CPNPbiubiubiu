f68043dca5fab5fa6169eb329ced9275ac9ed85e
youryuu <l9819y@gmail.com>
2022-01-09 13:56:47 +0800

    ***********背包问题

--- LeetCode/leetcode494.cpp
+++ LeetCode/leetcode494.cpp
@@ -20,3 +20,26 @@ int count = 0;
         return count;
         }
 };
+class Solution {
+public:
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        int len = nums.size();
+        for(int i=0; i<len; ++i)
+            sum += nums[i];
+        if(sum < target || (sum - target)%2!=0)    return 0;
+        int neg = (sum - target)/2;     //在nums中找到和为neg的数量
+        vector<int> dp(neg+1);
+        dp[0] = 1;
+        for(auto num : nums)
+        {
+            for(int i=neg; i>=num; --i)
+            {
+                dp[i] = dp[i] + dp[i - num];
+            }
+        }
+        return dp[neg];
+        }
+};
+
