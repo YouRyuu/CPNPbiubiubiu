3c5cc372792a1c0d41d19181a7a8963e0195a489
youryuu <l9819y@gmail.com>
2022-01-10 21:48:59 +0800

    反转链表递归版的解释

--- LeetCode/reverse-linked-list.c
+++ LeetCode/reverse-linked-list.c
@@ -28,3 +28,10 @@ struct ListNode* reverseList(struct ListNode* head) {
     head->next = NULL;
     return p;
 }
+/*
+对递归版本的解释：
+存在链表：p1->p2->p3->p4
+reverseList(p1):p=p4,p1->next->next=p1,p1->next=null,(p1<-p2<-p3<-p4)
+	reverseList(p2):p=p4, p2->next->next=p2, p2->next=null.(p1->p2<-p3<-p4)
+		reverList(p3):p=p4, p3->next->next=p3, p3->next=null.(p1->p2->p3<-p4;
+			reverList(p4):return p4
