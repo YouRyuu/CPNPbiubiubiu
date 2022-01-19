6e0dc5813697a22cd977bf8a78d128e1dbecadd8
youryuu <l9819y@gmail.com>
2022-01-19 19:25:02 +0800

    set map

--- LeetCode/leetcode219.cpp
+++ LeetCode/leetcode219.cpp
@@ -13,3 +13,20 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int size = nums.size();
+        unordered_set<int> ret;
+        for(int i=0;i<size;i++)
+        {
+            if(i>k)
+                ret.erase(nums[i-k-1]);
+            if(ret.count(nums[i])!=0)
+                return true;
+            ret.emplace(nums[i]);
+        }
+        return false;
+    }
+};
