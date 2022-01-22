4609895c04088d2c64ff335499c1b3d69c34062b
youryuu <l9819y@gmail.com>
2022-01-22 16:27:38 +0800

    使用最小花费爬楼梯

---
+++ LeetCode/leetcode746.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int size = cost.size();
+        vector<int> dp(size+1);
+        dp[0] = 0, dp[1] = 0;
+        for(int i=2;i<=size;++i)
+        {
+            dp[i] = min(dp[i-2]+cost[i-2], dp[i-1]+cost[i-1]);
+        }
+        return dp[size];
+
+    }
+};
