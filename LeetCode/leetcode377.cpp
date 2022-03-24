acb36efba1dc618af5a8667765c243624db2001d
youryuu <l9819y@gmail.com>
2022-03-24 22:27:04 +0800

    组合总数4*

---
+++ LeetCode/leetcode377.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=target; ++i)
+        {
+            for(int j=0;j<len;++j)
+            {
+                if(nums[j]<=i && dp[i - nums[j]] < INT_MAX - dp[i])
+                {
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
