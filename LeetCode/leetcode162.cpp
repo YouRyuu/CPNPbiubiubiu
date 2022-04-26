1d1c0ef7deb8d6f36e7c7b8e3ef6bce8c77eaef0
youryuu <l9819y@gmail.com>
2022-04-26 13:26:57 +0800

    寻找峰值*

---
+++ LeetCode/leetcode162.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int len = nums.size();
+        int left = 0, right = len-1;
+        while(left < right)
+        {
+            int mid = left + (right - left)/2;
+            if(nums[mid] < nums[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+};
