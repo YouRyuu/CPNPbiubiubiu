9b18a0eabfe7b8f645ca05279deb95e5cb4c98ed
youryuu <l9819y@gmail.com>
2022-04-27 15:04:27 +0800

    最大数*

---
+++ LeetCode/leetcode179.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](int x, int y){
+            long sx=10, sy=10;
+            while(sx<=x)
+                sx *= 10;
+            while(sy<=y)
+                sy *= 10;
+            return sy*x+y > sx*y+x;
+        });
+        if(nums[0]==0)
+            return "0";
+        string ret;
+        for(auto num:nums)
+        {
+            ret += to_string(num);
+        }
+        return ret;
+    }
+};
