1e8a670311156538d0c1bb961a7a13673a78dba8
youryuu <l9819y@gmail.com>
2022-05-01 09:53:25 +0800

    两个数组的交集2,两种解法

--- LeetCode/leetcode350.cpp
+++ LeetCode/leetcode350.cpp
@@ -24,3 +24,27 @@ public:
         return ret;
     }
 };
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+        int i=0, j=0;
+        while(i<len1 && j<len2)
+        {
+            if(nums1[i] < nums2[j])
+                ++i;
+            else if(nums1[i] > nums2[j])
+                ++j;
+            else
+            {
+                ret.push_back(nums1[i]);
+                ++i;++j;
+            }
+        }
+        return ret;
+    }
+};
