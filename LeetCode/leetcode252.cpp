48174d59d1e777ab6d88010852387215d168bba8
youryuu <l9819y@gmail.com>
2022-05-03 15:44:39 +0800

    会议室

---
+++ LeetCode/leetcode252.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        int len = intervals.size();
+        if(len < 2)
+            return true;
+        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b)
+        {
+            return a[0] < b[0];
+        });
+        for(int i=1; i<len; ++i)
+        {
+            if(intervals[i][0] < intervals[i-1][1])
+                return false;
+        }
+        return true;
+    }
+};
