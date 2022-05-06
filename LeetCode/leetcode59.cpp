be447820e3dd73015f973836d930af219752d3c4
youryuu <l9819y@gmail.com>
2022-05-06 09:49:44 +0800

    螺旋矩阵2*

--- LeetCode/leetcode59.cpp
+++ LeetCode/leetcode59.cpp
@@ -21,3 +21,31 @@ public:
         return ret;
     }
 };
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ret(n, vector<int>(n, 0));
+        int val = 1;
+        int left = 0, right = n-1, top = 0, buttom = n-1;
+        while(left <= right && top<=buttom)
+        {
+            for(int col = left; col<=right; ++col)
+                ret[top][col] = val++;
+            for(int row = top+1; row<=buttom; ++row)
+                ret[row][right] = val++;
+            //if(left < right && top<buttom)
+            //{
+                for(int col = right-1; col>left; --col)
+                    ret[buttom][col] = val++;
+                for(int row = buttom; row>top;--row)
+                    ret[row][left] = val++;
+           // }
+            
+            ++left;
+            ++top;
+            --right;
+            --buttom;
+        }
+        return ret;
+    }
+};
