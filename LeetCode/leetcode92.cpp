509903adc0bd256964b060b190466db573518d53
youryuu <l9819y@gmail.com>
2022-05-06 11:10:08 +0800

    反转链表2

--- LeetCode/leetcode92.cpp
+++ LeetCode/leetcode92.cpp
@@ -48,3 +48,35 @@ public:
 
     }
 };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode *hair = new ListNode;
+        hair->next = head;
+        ListNode *pre = hair;
+        for(int i=0; i<left-1; ++i)
+        {
+            pre = pre->next;
+        }
+        ListNode *curr = pre->next;
+        for(int i=0; i<right-left; ++i)
+        {
+            ListNode *next = curr->next;
+            curr->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+
+        }
+        return hair->next;
+    }
+};
