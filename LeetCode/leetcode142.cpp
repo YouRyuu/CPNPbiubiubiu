26151b6644676a53bbf6f851e328339bd43bba28
youryuu <l9819y@gmail.com>
2022-05-08 14:28:19 +0800

    环形链表2  快慢指针法

--- LeetCode/leetcode142.cpp
+++ LeetCode/leetcode142.cpp
@@ -29,3 +29,36 @@ public:
         return NULL;
     }
 };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while(fast!=NULL)
+        {
+            fast = fast->next;
+            if(fast==NULL)
+                return NULL;
+            slow = slow->next;
+            fast = fast->next;
+            if(slow == fast)
+            {
+                ListNode *ptr = head;
+                while(slow!=ptr)
+                {
+                    slow = slow->next;
+                    ptr = ptr->next;
+                }
+                return ptr;
+            }
+        }
+        return NULL;
+    }
+};
