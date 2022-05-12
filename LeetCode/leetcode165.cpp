71c95532e83aeaba444d12dd1815cac8c8e80388
youryuu <l9819y@gmail.com>
2022-05-12 11:07:57 +0800

    比较版本号

---
+++ LeetCode/leetcode165.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n = version1.size(), m = version2.size();
+        int i=0, j=0;
+        while(i<n || j<m)
+        {
+            unsigned int x = 0;
+            for(; i<n && version1[i]!='.'; ++i)
+            {
+                x = x*10 + version1[i]-'0';
+            }
+            ++i;
+            unsigned int y=0;
+            for(;j<m && version2[j]!='.'; ++j)
+                y = y*10 + version2[j]-'0';
+            ++j;
+            if(x!=y)
+                return x>y?1:-1;
+        }
+        return 0;
+    }
+};
+
