eb9ba8e5d8ddcde146c9e87d8557761b5dca696a
youryuu <l9819y@gmail.com>
2022-07-10 19:06:51 +0800

    接雨水，单调栈

--- LeetCode/leetcode42.cpp
+++ LeetCode/leetcode42.cpp
@@ -18,4 +18,25 @@ public:
         return ans;
     }
 };
-
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ret = 0;
+        int len = height.size();
+        stack<int> stk;
+        for(int i=0; i<len; ++i)
+        {
+            while(!stk.empty() && height[i] > height[stk.top()])
+            {
+                int v = stk.top();
+                stk.pop();
+                if(stk.empty())
+                    break;
+                int area = (i-stk.top()-1) * (min(height[i], height[stk.top()])-height[v]);
+                ret += area;
+            }
+            stk.push(i);
+        }
+        return ret;
+    }
+};
